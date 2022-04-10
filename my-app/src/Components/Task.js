bc52fd4219795ca231283caf8f88d0482d79b4c5
Sage Mahannah <mahan22s@mtholyoke.edu>
2022-04-10 01:42:23 -0400

    Added BootStrap modal behavior to task items

--- my-app/src/Components/Task.js
+++ my-app/src/Components/Task.js
@@ -5,7 +5,24 @@ function Task(props) {
   return (
     <div className="Task">
       <input type="checkbox">
-      <label>props.description</label>
+      <label><a data-toggle="modal" data-target="#modalCenter">props.description</a></label><br>
+
+      <!-- Modal -->
+      <div class="modal fade" id="modalCenter" tabindex="-1" role="dialog" aria-labelledby="modalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="modalLongTitle">props.title</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              props.info
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
