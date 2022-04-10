cb45ae443e3d2c2b5ec3286b6c80e3197ab06da6
Sage Mahannah <mahan22s@mtholyoke.edu>
2022-04-10 01:53:43 -0400

    Fixing Task to respond to parameters

--- my-app/src/Components/Task.js
+++ my-app/src/Components/Task.js
@@ -5,20 +5,20 @@ function Task(props) {
   return (
     <div className="Task">
       <input type="checkbox">
-      <label><a data-toggle="modal" data-target="#modalCenter">props.description</a></label><br>
+      <label><a data-toggle="modal" data-target="#modalCenter">{props.description}</a></label><br>
 
       <!-- Modal -->
       <div class="modal fade" id="modalCenter" tabindex="-1" role="dialog" aria-labelledby="modalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="modalLongTitle">props.title</h5>
+              <h5 class="modal-title" id="modalLongTitle">{props.title}</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div class="modal-body">
-              props.info
+              {props.info}
             </div>
           </div>
         </div>
